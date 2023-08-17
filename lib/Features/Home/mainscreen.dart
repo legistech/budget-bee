d9cfd1efa04ad1a529f59b3fb3865b786b5ee3ef
Haxeeb71 <135656763+Haxeeb71@users.noreply.github.com>
2023-08-17 18:38:14 +0500

    bottombar1

---
+++ lib/Features/Home/mainscreen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 0;
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Home',
+    ),
+    Text(
+      'Category',
+    ),
+    Text(
+      'Cart',
+    ),
+    Text(
+      'Favourite Products',
+    ),
+    Text(
+      'Profile Settings',
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        iconSize: 24,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.house),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.shapes),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.cartShopping),
+            label: 'School',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.solidHeart),
+            label: 'School',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.userLarge),
+            label: 'School',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
